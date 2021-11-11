196539658bdb00604ca965481a97d34778eacab6
Younes <47226184+younesaassila@users.noreply.github.com>
2021-11-11 12:19:56 +0100

    ✨ Add `VLiteral` support and fix bugs

--- src/rules/smart-apostrophes.ts
+++ src/rules/smart-apostrophes.ts
@@ -12,25 +12,26 @@ const rule: Rule.RuleModule = {
     schema: [],
   },
   create: (context) => {
-    function handle(node: Node) {
+    function handle(node: AST.Node | Node) {
       if (!("value" in node)) return;
 
       const value = node.value;
 
       if (typeof value !== "string") return;
 
-      const straightApostropheRegex = /(\p{Letter})'(\p{Letter})/gu;
+      const straightApostropheRegex = /(\p{Letter})(?:\\)?'(\p{Letter})/gu;
       if (!straightApostropheRegex.test(value)) return;
 
       context.report({
-        node,
-        message: "Prefer the use of a smart apostrophe (’).",
+        node: node as Node,
+        message: "Prefer the use of smart apostrophes (’).",
         fix(fixer) {
           const isLiteral = node.type === "Literal";
+          const isVLiteral = node.type === "VLiteral";
 
           let text: string;
-          if (isLiteral) {
-            // Remove string delimiters to avoid any potential issue with quote replacing.
+          if (isLiteral && "raw" in node) {
+            // Remove string delimiters to avoid any potential issues with replacing quotes.
             text = node.raw.slice(1, node.raw.length - 1);
           } else {
             text = value;
@@ -40,42 +41,42 @@ const rule: Rule.RuleModule = {
             straightApostropheRegex,
             (_, a, b) => `${a}’${b}`
           );
-
-          if (isLiteral) {
+          if (isLiteral && "raw" in node) {
             // Add string delimiters back.
             fixedText =
               node.raw.charAt(0) +
               fixedText +
               node.raw.charAt(node.raw.length - 1);
+          } else if (isVLiteral) {
+            // VLiteral nodes require the fixed text to have string delimiters
+            // even though their initial value don't have any.
+            fixedText = `"${fixedText}"`;
           }
 
-          return fixer.replaceText(node, fixedText);
+          return fixer.replaceText(node as Node, fixedText);
         },
       });
     }
 
-    // TODO: Fix VLiteral and type issues.
+    // Vue.js
+    if (context.parserServices.defineTemplateBodyVisitor) {
+      return context.parserServices.defineTemplateBodyVisitor(
+        // Event handlers for <template>.
+        {
+          Literal: (node: AST.ESLintLiteral) => handle(node),
+          VLiteral: (node: AST.VLiteral) => handle(node),
+          VText: (node: AST.VText) => handle(node),
+        },
+        // Event handlers for <script> or scripts.
+        {
+          Literal: (node: AST.ESLintLiteral) => handle(node),
+        }
+      );
+    }
 
-    if (!context.parserServices.defineTemplateBodyVisitor)
-      return {
-        Literal: (node: Literal) => handle(node),
-      };
-
-    return context.parserServices.defineTemplateBodyVisitor(
-      // Event handlers for <template>.
-      {
-        // @ts-ignore
-        Literal: (node: AST.ESLintLiteral) => handle(node),
-        // VLiteral: (node: AST.VLiteral) => handle(node),
-        // @ts-ignore
-        VText: (node: AST.VText) => handle(node),
-      },
-      // Event handlers for <script> or scripts.
-      {
-        // @ts-ignore
-        Literal: (node: AST.ESLintLiteral) => handle(node),
-      }
-    );
+    return {
+      Literal: (node: Literal) => handle(node),
+    };
   },
 };
 
